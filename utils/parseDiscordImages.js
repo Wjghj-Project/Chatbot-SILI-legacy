631f64cd4b639bcd5df026db200e1d0fffde084c
Dragon-Fish <824399619@qq.com>
2021-03-03 01:13:09 +0800

    Minor fix

--- utils/parseDiscordImages.js
+++ utils/parseDiscordImages.js
@@ -10,16 +10,17 @@ module.exports = ({ msg, content }) => {
         // content += '[CQ:image,file=' + img + ']'
         // content += img
         content += `[附图] ${img}`
+        axios.post(require('../secret/discord').fandom_zh.webhook, {
+          username: 'SILI',
+          content:
+            '[INFO] 由于政策原因，QQ 用户无法看到来自 Discord 的图片，请使用图床等工具展示图片。',
+          avatar_url: 'https://i.loli.net/2021/03/01/BX9mLQdxaTzYEV3.jpg',
+        })
       }
     } catch (e) {
       console.error('转换图片出错', e)
     }
   }
-  axios.post(require('../secret/discord').fandom_zh.webhook, {
-    username: 'SILI',
-    content:
-      '[INFO] 由于政策原因，QQ 用户无法看到来自 Discord 的图片，请使用图床等工具展示图片。',
-    avatar_url: 'https://i.loli.net/2021/03/01/BX9mLQdxaTzYEV3.jpg',
-  })
+
   return content
 }
