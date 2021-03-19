7c88f8c735d35770002872e4c4f7177d7af0ff4b
Dragon-Fish <824399619@qq.com>
2021-03-20 01:15:00 +0800

    + plugin-status

--- modules/loadPlugins.js
+++ modules/loadPlugins.js
@@ -7,68 +7,6 @@ const path = require('path')
  * @module loadPlugins 插件配置
  */
 module.exports = () => {
-  koishi.plugin(require('koishi-plugin-common'), {
-    // 复读机
-    onRepeat(state, session) {
-      let repeatCD = 30
-      globalThis.repeaterLast = globalThis.repeaterLast || 0
-      let now = new Date().getTime()
-
-      // 参与复读
-      if (
-        state.times === 3 &&
-        now - globalThis.repeaterLast > repeatCD * 1000 &&
-        1 - Math.random() > 0.8
-      ) {
-        globalThis.repeaterLast = now
-        return state.content
-      }
-
-      // 冷却未好
-      else if (state.times === 3) {
-        session.logger('复读机').info('距离上次复读不足' + repeatCD + '秒')
-      }
-
-      // 大于 5 次，打断复读
-      if (state.times > 5) {
-        return segment('image', {
-          file: `file:///${path.resolve('./images/no_repeat.jpg')}`,
-        })
-      }
-    },
-    // repeater: {
-    // 3次自动复读，并设置30秒cd
-    // repeat: (repeated, times) => {
-    //   let repeatCD = 30
-    //   globalThis.repeaterLast = globalThis.repeaterLast || 0
-    //   let now = new Date().getTime()
-    //   if (times === 3 && now - globalThis.repeaterLast > repeatCD * 1000) {
-    //     globalThis.repeaterLast = now
-    //     return true
-    //   } else if (times === 3) {
-    //     console.log('距离上次复读不足' + repeatCD + '秒')
-    //   }
-    // },
-    // // 复读大于5次打断
-    // interrupt: (_, times /*, message*/) => times > 5,
-    // interruptText: `[CQ:image,file=file:///${path.resolve(
-    //   './images/no_repeat.jpg'
-    // )}]`,
-    // },
-  })
-  koishi.plugin(require('koishi-plugin-teach'), {
-    prefix: '?!',
-  })
-  // koishi.plugin(require('koishi-plugin-schedule'))
-  koishi.plugin(require('koishi-plugin-genshin'), {
-    cookie: password.mhyCookie,
-  })
-  koishi.plugin(require('koishi-plugin-tools'))
-  koishi.plugin(require('koishi-plugin-image-search'))
-  koishi.plugin(require('../plugins/dbadmin'))
-  koishi.plugin(require('../plugins/recall'))
-  koishi.plugin(require('../plugins/youdao'))
-  // koishi.plugin(require('koishi-plugin-status'), {})
   koishi.plugin(require('koishi-plugin-blame'), {
     catch: ['unhandledRejection'],
     send: {
@@ -77,4 +15,54 @@ module.exports = () => {
     },
     sender: ['onebot:' + require('../secret/qqNumber').user.mySelf],
   })
+  koishi.plugin(require('koishi-plugin-common'), {
+    // 复读机
+    onRepeat(state) {
+      let repeatCD = 30 * 1000
+      globalThis.repeaterLast = globalThis.repeaterLast || 0
+      let now = new Date().getTime()
+
+      // 参与复读
+      if (state.times === 3 && now - globalThis.repeaterLast > repeatCD) {
+        if (1 - Math.random() > 0.8) {
+          globalThis.repeaterLast = now
+          return state.content
+        } else {
+          koishi.logger('复读机').info('概率未触发复读机')
+        }
+      }
+
+      // 冷却未好
+      else if (state.times === 3) {
+        koishi.logger('复读机').info('距离上次复读不足' + repeatCD + '秒')
+      }
+
+      // 大于 5 次，打断复读
+      if (state.times > 5) {
+        return (
+          segment('image', {
+            file: `file:///${path.resolve('./images/no_repeat.jpg')}`,
+          }) + 'No，不要再复读了！'
+        )
+      }
+    },
+  })
+  koishi.plugin(require('koishi-plugin-genshin'), {
+    cookie: password.mhyCookie,
+  })
+  koishi.plugin(require('koishi-plugin-image-search'))
+  koishi.plugin(require('koishi-plugin-schedule'))
+  koishi.plugin(require('koishi-plugin-status'), {
+    path: '/status',
+    port: 8080,
+  })
+  koishi.plugin(require('koishi-plugin-teach'), {
+    prefix: '?!',
+  })
+  koishi.plugin(require('koishi-plugin-tools'))
+
+  // Local plugins
+  koishi.plugin(require('../plugins/dbadmin'))
+  koishi.plugin(require('../plugins/recall'))
+  koishi.plugin(require('../plugins/youdao'))
 }
