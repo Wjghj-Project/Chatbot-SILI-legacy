029b46e8cb83d3bcbba3f29a6e0d1d025769616a
Winston Sung <52590365+WinstonSung@users.noreply.github.com>
2021-05-03 01:04:58 +0800

    update(util.resolveBrackets): fix &amp; (#2)
    LGTM

--- utils/resolveBrackets.js
+++ utils/resolveBrackets.js
@@ -2,5 +2,6 @@ module.exports = str => {
   str = str
     .replace(new RegExp('&#91;', 'g'), '[')
     .replace(new RegExp('&#93;', 'g'), ']')
+    .replace(new RegExp('&amp;', 'g'), '&')
   return str
 }
