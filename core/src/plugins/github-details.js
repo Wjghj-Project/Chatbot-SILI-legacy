54f19cc13e95c2da8f07eab5271e6b5d5d3223e9
Dragon-Fish <824399619@qq.com>
2021-10-24 22:19:41 +0800

    chore: minor fix (bump version: 3.13.1)

--- core/src/plugins/github-details.js
+++ core/src/plugins/github-details.js
@@ -6,8 +6,11 @@ const { segment } = require('koishi-utils')
  */
 function apply(ctx) {
   ctx
-    .command('github.details', '<target:text> Get github user/repo details')
+    .command('github.details <target:text>', '查看 GitHub 用户/仓库信息摘要')
+    .example(`用户或组织: github.details Username`)
+    .example('仓库: github.details Owner/Repo')
     .action(async ({ session }, target) => {
+      if (!target) return session.execute('help github.details')
       if (target.includes('/')) {
         return session.execute(`github.details.repo ${target}`)
       } else {
@@ -16,8 +19,9 @@ function apply(ctx) {
     })
 
   ctx
-    .command('github.details.user', '<user:text>', { hidden: true })
+    .command('github.details.user <user:text>', '', { hidden: true })
     .action(async ({ session }, user) => {
+      if (!user) return
       let data
       try {
         const res = await axios.get(
@@ -26,58 +30,56 @@ function apply(ctx) {
         )
         data = res.data
       } catch (err) {
+        if (err?.response?.status === 404) {
+          return `用户或组织 ${user} 似乎不存在。`
+        }
+
         ctx.logger('ghDetails').warn(err)
         return `暂时无法连接到 GitHub${
           err.message ? '\n(' + err.message + ')' : ''
         }`
       }
 
-      const {
-        type,
-        login,
-        avatar_url,
-        name,
-        html_url,
-        blog,
-        bio,
-        created_at,
-        updated_at,
-      } = data
-      session.send(
-        segment('quote', { id: session.messageId }) +
-          [
-            segment.image(avatar_url),
-            `${type === 'Organization' ? '[组织]' : ''}${name})`,
-            bio,
-            html_url,
-            `主页：${blog ? blog : '无'}`,
-            `注册于 ${new Date(
-              created_at
-            ).toLocaleString()}，最近活跃于 ${new Date(
-              updated_at
-            ).toLocaleString()}`,
-          ].join('\n')
-      )
+      const { type, login, html_url, blog, created_at, updated_at } = data
 
       const page = await ctx.puppeteer.page()
+      let readme
       try {
         await page.goto(`https://hub.fastgit.org/${login}`)
         const $readme = await page.$(
           type === 'Organization' ? '.orghead' : '.js-profile-editable-replace'
         )
-        const readme = await $readme.screenshot({
+        readme = await $readme.screenshot({
           type: 'jpeg',
         })
-        await session.send(segment.image(readme))
+        await page.close()
       } catch (err) {
+        await page.close()
         ctx.logger('ghDetails').warn('user 截图失败', err)
+        return `暂时无法连接到 GitHub${
+          err.message ? '\n(' + err.message + ')' : ''
+        }`
       }
-      await page.close()
+
+      return (
+        segment('quote', { id: session.messageId }) +
+        [
+          segment.image(readme),
+          html_url,
+          `主页：${blog ? blog : '无'}`,
+          `注册于 ${new Date(
+            created_at
+          ).toLocaleString()}，最近活跃于 ${new Date(
+            updated_at
+          ).toLocaleString()}`,
+        ].join('\n')
+      )
     })
 
   ctx
-    .command('github.details.repo', '<repo:text>', { hidden: true })
+    .command('github.details.repo <repo:text>', '', { hidden: true })
     .action(async ({ session }, repo) => {
+      if (!repo) return
       let data
       try {
         const res = await axios.get(
@@ -86,6 +88,10 @@ function apply(ctx) {
         )
         data = res.data
       } catch (err) {
+        if (err?.response?.status === 404) {
+          return `仓库 ${repo} 似乎不存在。`
+        }
+
         ctx.logger('ghDetails').warn(err)
         return `暂时无法连接到 GitHub${
           err.message ? '\n(' + err.message + ')' : ''
@@ -123,13 +129,12 @@ function apply(ctx) {
 
   ctx.middleware(async (session, next) => {
     await next()
-    const ghReg = /^(?:https?:\/\/)?github\.com\/(.+?)\/(.+)?/i
+    const ghReg = /^(?:https?:\/\/)?github\.com\/(.+)/i
     const ghTest = ghReg.exec(session.content)
     if (!ghTest) return
     ghTest.shift()
-    const [owner, repo] = ghTest
-
-    session.execute(`github.details ${owner}${repo ? '/' + repo : ''}`)
+    const [target] = ghTest
+    session.execute(`github.details ${target.split(/[\n\s]/).shift()}`)
   })
 }
 
